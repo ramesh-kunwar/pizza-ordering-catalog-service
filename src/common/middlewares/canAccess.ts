a5ed612015b99bbe494aaac2ec3f2fbdaa39a241
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-07-29 08:51:02 +0545

    add authorization

---
+++ src/common/middlewares/canAccess.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
+import { AuthRequest } from "../types";
+
+export const canAccess = (roles: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const _req = req as AuthRequest;
+
+        const roleFromToken = _req.auth.role;
+
+        if (!roles.includes(roleFromToken)) {
+            const error = createHttpError(
+                403,
+                "You don't have enough permissions.",
+            );
+
+            next(error);
+            return;
+        }
+        next();
+    };
+};
