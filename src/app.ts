35c888a9090043047edbc7421b767eda14b3c3b2
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-08-04 10:35:00 +0545

    add frontend url to solve cors issue

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: [config.get("FRONT_END_URL")],
+        origin: config.get("FRONT_END_URLS"),
         credentials: true,
     }),
 );
